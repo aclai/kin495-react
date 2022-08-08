86e12a65a29ef88dbc93f7dd38567d55c9a1261a
Andy <andy_lai314@hotmail.com>
2022-08-08 00:42:35 -0700

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	  <img src={require('./assets/kin495.svg')} alt={'kin495logo'} />
+	  <p>hi</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
